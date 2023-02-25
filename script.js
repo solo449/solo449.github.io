e6c4727ddfe5c14e37d5ee5f4a9b3698e43b59ac
solo449 <65399252+solo449@users.noreply.github.com>
2023-02-24 22:47:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ $(document).ready(function() {
 		event.preventDefault();
 		
 		// get the values of the username and message fields
-		var username = $('#username
-                     
+		var username = $('#username').val();
+		var message = $('#message').val();
+		
+		// make an AJAX POST request to the server with the username and message data
+		$.ajax({
+			url: '/messages',
+			type: 'POST',
+			data: { username: username, message: message },
+			success: function(response) {
+				// if the server responds with a success message, add the new message to the messages list
+				$('#messages').append('<li><strong>' + username + ':</strong> ' + message + '</li>');
+				// clear the form fields
+				$('#username').val('');
+				$('#message').val('');
+			},
+			error: function(error) {
+				// if the server

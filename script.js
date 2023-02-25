07264286994eccb6161219ca6872692dafaeb590
solo449 <65399252+solo449@users.noreply.github.com>
2023-02-24 22:43:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+	// when the document is ready, add a listener to the form submission event
+	$('#message-form').on('submit', function(event) {
+		// prevent the default form submission behavior
+		event.preventDefault();
+		
+		// get the values of the username and message fields
+		var username = $('#username
+                     

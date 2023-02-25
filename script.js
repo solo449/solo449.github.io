d5b7faa8135fd4cdb24658907f1d68addccd53bb
solo449 <65399252+solo449@users.noreply.github.com>
2023-02-25 17:20:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
-	// when the document is ready, add a listener to the form submission event
-	$('#message-form').on('submit', function(event) {
-		// prevent the default form submission behavior
-		event.preventDefault();
-		
-		// get the values of the username and message fields
-		var username = $('#username').val();
-		var message = $('#message').val();
-		
-		// make an AJAX POST request to the server with the username and message data
-		$.ajax({
-			url: '/messages',
-			type: 'POST',
-			data: { username: username, message: message },
-			success: function(response) {
-				// if the server responds with a success message, add the new message to the messages list
-				$('#messages').append('<li><strong>' + username + ':</strong> ' + message + '</li>');
-				// clear the form fields
-				$('#username').val('');
-				$('#message').val('');
-			},
-			error: function(error) {
-				// if the server
+const form = document.querySelector('form');
+const commentList = document.querySelector('#comment-list');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = event.target.elements.name.value;
+  const comment = event.target.elements.comment.value;
+
+  const li = document.createElement('li');
+  const nameSpan = document.createElement('span');
+  const commentSpan = document.createElement('span');
+
+  nameSpan.textContent = `${name}: `;
+  commentSpan.textContent = comment;
+
+  li.appendChild(nameSpan);
+  li.appendChild(commentSpan);
+
+  commentList.appendChild(li);
+
+  event.target.reset();
+});
